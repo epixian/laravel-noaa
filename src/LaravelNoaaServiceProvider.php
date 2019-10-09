04ffd22adb948a8e18cba72a807b1397e68be77c
epixian <epixian@epixian.com>
2019-10-09 14:23:18 -0400

    Create LaravelNoaaServiceProvider.php

---
+++ src/LaravelNoaaServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Epixian\LaravelNoaa;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelNoaaServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     * 
+     * @return void
+     */
+    public function boot()
+    {
+        $source = dirname(__DIR__) . '/config/noaa.php';
+
+        $this->publishes([$source => config_path('noaa.php')]);
+
+        $this->mergeConfigFrom($source, 'noaa');
+    }
+
+    /**
+     * Register package services.
+     * 
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('noaa', function ($app) {
+            return new Noaa();
+        });
+    }
+}
